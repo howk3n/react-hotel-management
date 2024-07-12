547bfde3243412c463ad40f963b152a9ab535eca
eriukiyo <dusancvijic1906@gmail.com>
2024-07-12 18:33:10 +0200

    Reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/globalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -19,11 +14,25 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hotel Management</H1>
-        <Button>Check in</Button>
-        <Button>Check out</Button>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hotel Management</Heading>
+            <div>
+              <Heading as="h2">Check In/Out</Heading>
+              <Button>Check in</Button>
+              <Button variation="secondary" size="small">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
