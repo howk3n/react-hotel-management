54dd9693061fb3290eb6131a314369e6ed6a5b0b
eriukiyo <dusancvijic1906@gmail.com>
2024-08-07 23:11:47 +0200

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'react-app',
-    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
